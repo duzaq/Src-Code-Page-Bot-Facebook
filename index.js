e6bd439d8a615d34107faa38a65cbd939a0605a2
duzaq <duzaq@live.com>
2024-11-18 21:36:44 -0300

    Update index.js
    update comandos

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 const VERIFY_TOKEN = 'pagebot';
 const PAGE_ACCESS_TOKEN = fs.readFileSync('token.txt', 'utf8').trim();
-const COMMANDS_PATH = path.join(__dirname, 'commands');
+const COMMANDS_PATH = path.join(__dirname, 'commands2');
 
 // Webhook verification
 app.get('/webhook', (req, res) => {
